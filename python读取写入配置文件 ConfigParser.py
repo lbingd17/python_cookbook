4b57a53708bc19d46a9e99d004db158dfa5a7e69
timtimli <lbingd17@mail.ustc.edu.cn>
2019-03-25 09:15:22 +0800

    Update python读取写入配置文件 ConfigParser.py

--- python读取写入配置文件 ConfigParser.py
+++ python读取写入配置文件 ConfigParser.py
@@ -1,19 +1,25 @@
 
 https://www.cnblogs.com/feeland/p/4514771.html
 
-
- 　　基础读取配置文件
-
- 
-
+========================
+读取配置文件
 -read(filename)               直接读取文件内容
 -sections()                      得到所有的section，并以列表的形式返回
 -options(section)            得到该section的所有option
 -items(section)                得到该section的所有键值对
 -get(section,option)        得到section中option的值，返回为string类型
 -getint(section,option)    得到section中option的值，返回为int类型，还有相应的getboolean()和getfloat() 函数。
+====================  test.conf
+[db]
+db_port = 3306
+db_user = root
+db_host = 127.0.0.1
+db_pass = 3306
 
-
+[cocurrent]
+processor = 20
+thread = 10
+==================== myConfigParser.py
 # !/usr/bin/env python
 # -*- coding:utf-8 -*-  
 
@@ -51,4 +57,4 @@ print "db_pass:", db_pass
 print "thread:", threads
 print "processor:", processors
 
-ConfigParser
+ 
